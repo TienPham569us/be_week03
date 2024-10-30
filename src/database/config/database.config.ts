13e9a2b541dc50a10062efc6b06844827d37d051
Phạm Đình Tiến <136001495+TienPham569us@users.noreply.github.com>
2024-10-30 11:49:52 +0700

    add module dialect

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import { IDatabaseConfig } from './interfaces/dbConfig.interface';
+import * as pg from 'pg';
 
 dotenv.config();
 
@@ -10,7 +11,8 @@ export const databaseConfig: IDatabaseConfig = {
     database: process.env.DB_NAME_DEVELOPMENT,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectModule: pg
   },
   test: {
     username: process.env.DB_USER,
@@ -18,13 +20,15 @@ export const databaseConfig: IDatabaseConfig = {
     database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectModule: pg
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME_PRODUCTION,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectModule: pg
   }
 };
